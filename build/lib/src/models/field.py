63daa03161df4fcea2b8156ce42b6348ee53f5cb
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2024-11-12 17:08:22 +0000

    removed build

--- build/lib/src/models/field.py
+++
@@ -1,6 +0,0 @@
-# Базовий клас Field, який представляє загальне поле даних
-class Field:
-    # Ініціалізатор класу Field
-    def __init__(self, value):
-        # Зберігає значення поля
-        self.value = value
